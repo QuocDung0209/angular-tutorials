db4b957624cbc0fee9e2a28549892cceab730af0
DESKTOP-8PHTJR9\Kenji <quocdung02091997@gmail.com>
2021-08-15 23:10:07 +0700

    [Deployment] Add Script to deploy project on Surge.sh

---
+++ deploy-surge.sh
@@ -0,0 +1,15 @@
+# Build application for production
+yarn build
+# npm run build # For NPM package manager
+
+# Move to build folder
+# dist/custom-directive-angular is "outputPath" in angular.json
+cd dist/custom-directive-angular
+
+# Clone index.html into 200.html
+# This helps solve the "Page not found" issue when refreshing page with routing
+cp index.html 200.html
+
+# Start deploying via Surege
+# The command means deploy current folder to domain angulardemo.surge.sh
+surge . angulardemo.surge.sh
