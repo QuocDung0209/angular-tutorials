26f39585aa723219a03af4bd6476ad160cad8781
DESKTOP-8PHTJR9\Kenji <quocdung02091997@gmail.com>
2021-08-15 17:16:57 +0700

    [Tooltip directive] Add Code Block

--- src/app/components/tooltip-directive/tooltip-directive.component.ts
+++ src/app/components/tooltip-directive/tooltip-directive.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TOOLTIP_TS, TOOLTIP_CSS } from 'src/app/constants/tooltip-directive.constant';
+import hljs from 'highlight.js/lib/common';
 
 @Component({
   selector: 'app-tooltip-directive',
   templateUrl: './tooltip-directive.component.html',
-  styleUrls: ['./tooltip-directive.component.scss']
+  styleUrls: ['./tooltip-directive.component.scss'],
 })
 export class TooltipDirectiveComponent implements OnInit {
   title: string = "Tooltip - Attribute Directive";
+  tsCode = hljs.highlight(TOOLTIP_TS, { language: 'typescript' }).value;
+  cssCode = hljs.highlight(TOOLTIP_CSS, { language: 'css' }).value;
 
   constructor() { }
 
