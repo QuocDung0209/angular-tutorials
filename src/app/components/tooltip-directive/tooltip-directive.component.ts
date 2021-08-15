10ba6126f34d6acc5e3c9e65a4cd3c21cef8c2f9
DESKTOP-8PHTJR9\Kenji <quocdung02091997@gmail.com>
2021-08-15 17:44:32 +0700

    [Tooltip Directive] Add HTML Tooltip

--- src/app/components/tooltip-directive/tooltip-directive.component.ts
+++ src/app/components/tooltip-directive/tooltip-directive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TOOLTIP_TS, TOOLTIP_CSS } from 'src/app/constants/tooltip-directive.constant';
+import { TOOLTIP_TS, TOOLTIP_CSS, HTML_TOOLTIP } from 'src/app/constants/tooltip-directive.constant';
 import hljs from 'highlight.js/lib/common';
 
 @Component({
@@ -11,6 +11,7 @@ export class TooltipDirectiveComponent implements OnInit {
   title: string = "Tooltip - Attribute Directive";
   tsCode = hljs.highlight(TOOLTIP_TS, { language: 'typescript' }).value;
   cssCode = hljs.highlight(TOOLTIP_CSS, { language: 'css' }).value;
+  htmlTooltip = HTML_TOOLTIP;
 
   constructor() { }
 
