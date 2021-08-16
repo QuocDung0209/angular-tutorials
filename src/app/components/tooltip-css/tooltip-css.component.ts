aa8a8187456faeb18feaafe0159838f202d14605
DESKTOP-8PHTJR9\Kenji <quocdung02091997@gmail.com>
2021-08-16 16:53:03 +0700

    [Tooltip CSS] Implement tooltip CSS using Popper

--- src/app/components/tooltip-css/tooltip-css.component.ts
+++ src/app/components/tooltip-css/tooltip-css.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { createPopper } from '@popperjs/core';
 
 @Component({
   selector: 'app-tooltip-css',
@@ -10,6 +11,46 @@ export class TooltipCssComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const button: any = document.querySelector('#button');
+    const tooltip: any = document.querySelector('#tooltip');
+
+    if (button && tooltip) {
+      const popperInstance = createPopper(button, tooltip, {
+        placement: 'top',
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 8],
+            },
+          },
+        ],
+      });
+
+      function show() {
+        // Make the tooltip visible
+        tooltip.setAttribute('data-show', '');
+
+        // Update its position
+        popperInstance.update();
+      }
+
+      function hide() {
+        // Hide the tooltip
+        tooltip.removeAttribute('data-show');
+      }
+
+      const showEvents = ['mouseenter', 'focus'];
+      const hideEvents = ['mouseleave', 'blur'];
+
+      showEvents.forEach(event => {
+        button.addEventListener(event, show);
+      });
+
+      hideEvents.forEach(event => {
+        button.addEventListener(event, hide);
+      });
+    }
   }
 
 }
