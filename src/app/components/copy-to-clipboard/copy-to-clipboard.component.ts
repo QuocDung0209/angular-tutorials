3c7875d911b95b191313812befc33be891f74c0d
DESKTOP-8PHTJR9\Kenji <quocdung02091997@gmail.com>
2021-08-18 19:00:45 +0700

    [Clipboard Directive] Add title

--- src/app/components/copy-to-clipboard/copy-to-clipboard.component.ts
+++ src/app/components/copy-to-clipboard/copy-to-clipboard.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./copy-to-clipboard.component.scss']
 })
 export class CopyToClipboardComponent implements OnInit {
+  title = "Create clipboard directive";
 
   constructor() { }
 
