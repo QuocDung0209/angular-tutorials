7e6d00649b57b0aec2ca87dc18556d0081a8d7b9
DESKTOP-8PHTJR9\Kenji <quocdung02091997@gmail.com>
2021-08-19 17:08:18 +0700

    [Upload file] Create drag & drop directive version 1

---
+++ src/app/directives/drag-drop.directive.ts
@@ -0,0 +1,49 @@
+import { Directive, Output, EventEmitter, HostBinding, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appDragDrop]'
+})
+export class DragDropDirective {
+  @HostBinding('class.fileover') fileOver!: boolean;
+  @Output() fileDropped = new EventEmitter<Array<File>>();
+  @Input() preventBodyDrop = true;
+
+  // Dragover listener
+  @HostListener('dragover', ['$event']) onDragOver(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = true;
+  }
+
+  // Dragleave listener
+  @HostListener('dragleave', ['$event']) public onDragLeave(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = false;
+  }
+
+  // Drop listener
+  @HostListener('drop', ['$event']) public ondrop(evt: any) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = false;
+    let files = evt.dataTransfer.files;
+    if (files.length > 0) {
+      this.fileDropped.emit(files);
+    }
+  }
+
+  @HostListener('body:dragover', ['$event'])
+  onBodyDragOver(event: DragEvent) {
+    if (this.preventBodyDrop) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  }
+  @HostListener('body:drop', ['$event'])
+  onBodyDrop(event: DragEvent) {
+    if (this.preventBodyDrop) {
+      event.preventDefault();
+    }
+  }
+}
