0cdcf8c2c96df94857b964a66d8badadf53f51a5
DESKTOP-8PHTJR9\Kenji <quocdung02091997@gmail.com>
2021-09-28 21:06:54 +0700

    [Angular Tutorials] Create Custom Pipe

--- src/app/angular-tutorials/pages/pipes-tutorial/pipes-tutorial.component.ts
+++ src/app/angular-tutorials/pages/pipes-tutorial/pipes-tutorial.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PipesTutorialComponent implements OnInit {
   asSyntax = `<div *ngIf=\"(hounds | async) as breeds\">\n  <ul>\n    <li *ngFor=\"let breed of breeds.message\">{{breed}}</li>\n  </ul> \n</div>`;
   usingShareReplay = `<div *ngIf=\"(obsValue | async); else elseBlock\">\n  {{ obsValue | async}}\n</div>`;
+  temparaturePipe = `import {Pipe, PipeTransform} from '@angular/core';\n \n@pipe({\n    name: 'tempConverter'\n})\nexport class TempConverterPipe implements PipeTransform {\n    transform(value: number, unit: string) {\n        if(value && !isNaN(value)) {\n            if (unit === 'C') {\n                var temperature = (value - 32) /1.8 ;\n                return temperature.toFixed(2);\n            } else if (unit === 'F'){\n                var temperature = (value * 1.8 ) + 32\n                return temperature.toFixed(2);\n            }\n        }\n        return;\n    }\n }`;
 
   constructor() {}
 
